8c103c03659b20bee0365a36283bd44dafc932a4
Jacob Tan <jacob.tan229@gmail.com>
2021-12-08 15:54:00 +1300

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header>
+      {/* left */}
+      <div>
+        <Image src="" layout="fill" />
+      </div>
+      {/* mid */}
+      <div></div>
+      {/* right */}
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
