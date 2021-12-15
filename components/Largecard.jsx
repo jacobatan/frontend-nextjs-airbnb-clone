8e74ec31e06348a8abcc02dab008a21799f2db2a
Jacob Tan <jacob.tan229@gmail.com>
2021-12-15 16:46:53 +1300

    removed the default padding on yaxis and made title semibold

--- components/Largecard.jsx
+++ components/Largecard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Largecard({ img, title, description, buttonText }) {
   return (
-    <section className="relative py-16 cursor-pointer ">
+    <section className="relative  cursor-pointer ">
       <div className="relative h-96 min-w-[300px]">
         <Image
           src={img}
@@ -14,7 +14,7 @@ function Largecard({ img, title, description, buttonText }) {
       </div>
 
       <div className="absolute top-32 left-12 max-w-xs">
-        <h3 className="text-4xl mb-3 w-64 text-white">{title}</h3>
+        <h3 className="text-4xl mb-3 w-64 text-white font-semibold">{title}</h3>
         <p className="text-white">{description}</p>
 
         <button
