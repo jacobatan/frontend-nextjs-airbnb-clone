0df03df264e063a078b284bab302f87d4e7bf195
Jacob Tan <jacob.tan229@gmail.com>
2021-12-15 16:46:39 +1300

    added Discover Airbnb Experiences

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,11 @@ export default function Home({ exploreData, cardsData }) {
       <Hero />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16 shadow-2xl my-5">
+        {/* SMALL CARDS / TRIP INSPIRATION  */}
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">
             Inspiration for your next trip
           </h2>
-
-          {/* pull data from server */}
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map(({ img, distance, location, id }) => (
@@ -35,23 +34,44 @@ export default function Home({ exploreData, cardsData }) {
             ))}
           </div>
         </section>
-
+        {/* MEDIUM CARDS / LIVE ANYWHERE */}
         <section>
           <h2 className="text-4xl font-semibold py-8">Live anywhere</h2>
-
           <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
             {cardsData.map(({ id, img, title }) => (
               <Mediumcard key={id} img={img} title={title} />
             ))}
           </div>
+        </section>{" "}
+        <div className="py-16">
+          <Largecard
+            img="https://images.pexels.com/photos/3760916/pexels-photo-3760916.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            title="Questions about hosting?"
+            description="Earn extra income and unlock new opportunities by sharing your space."
+            buttonText="Ask a superhost"
+          />
+        </div>
+        <section className="pb-16">
+          <h2 className="text-4xl font-semibold py-8">
+            Discover Airbnb Experiences
+          </h2>{" "}
+          <div className="grid grid-cols-2">
+            <div className="pr-4">
+              <Largecard
+                img="https://images.pexels.com/photos/8527681/pexels-photo-8527681.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                title="Things to do on your trip"
+                buttonText="Experiences"
+              />{" "}
+            </div>
+            <div className="pl-4">
+              <Largecard
+                img="https://images.pexels.com/photos/4012966/pexels-photo-4012966.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                title="Things to do from home"
+                buttonText="Online Experiences"
+              />{" "}
+            </div>
+          </div>
         </section>
-
-        <Largecard
-          img="https://images.pexels.com/photos/3760916/pexels-photo-3760916.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          title="Questions about hosting?"
-          description="Earn extra income and unlock new opportunities by sharing your space."
-          buttonText="Ask a superhost"
-        />
       </main>
       <Footer />
     </div>
