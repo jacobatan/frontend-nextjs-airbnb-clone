dc488b3e6d2268011d56ff9c4c09fa0882d1d202
Jacob Tan <jacob.tan229@gmail.com>
2021-12-15 13:23:55 +1300

    edited images for large card + changed text styles

--- components/Largecard.jsx
+++ components/Largecard.jsx
@@ -13,13 +13,13 @@ function Largecard({ img, title, description, buttonText }) {
         />
       </div>
 
-      <div className="absolute top-32 left-12">
-        <h3 className="text-4xl mb-3 w-64">{title}</h3>
-        <p>{description}</p>
+      <div className="absolute top-32 left-12 max-w-xs">
+        <h3 className="text-4xl mb-3 w-64 text-white">{title}</h3>
+        <p className="text-white">{description}</p>
 
         <button
-          className="text-sm text-white bg-gray-900 px-4 
-        py-2 rounded-lg mt-5"
+          className="text-sm text-gray-900 bg-white px-4 
+        py-2 rounded-lg mt-5 "
         >
           {buttonText}
         </button>
@@ -29,5 +29,3 @@ function Largecard({ img, title, description, buttonText }) {
 }
 
 export default Largecard;
-
-// src="https://a0.muscache.com/im/pictures/791aba62-2de8-4722-99b5-45838715eb34.jpg?im_q=medq&im_w=720"
